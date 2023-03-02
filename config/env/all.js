b41c31dfc21f4d3ee03aaca080859f4e25588d7e
kmarellano <arellanokurt0130@gmail.com>
2023-03-02 15:17:09 +0800

    fixes

---
+++ config/env/all.js
@@ -0,0 +1,13 @@
+// default app configuration
+const port = process.env.PORT || 4000;
+let db = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+
+module.exports = {
+	port,
+	db,
+	cookieSecret: "session_cookie_secret_key_here",
+	cryptoKey: "a_secure_key_for_crypto_here",
+	cryptoAlgo: "aes256",
+	hostName: "localhost",
+	environmentalScripts: [],
+};
